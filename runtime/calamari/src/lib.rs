2cf703b0137a7df5f2c172843d86af32e9be97d5
ferrell-code <charlie@manta.network>
2024-07-04 22:17:48 -0400

    Release v4.7.2 (#1347)
    * runtime metadta-hash in ci
    
    Signed-off-by: Charles Ferrell <charlie@manta.network>
    
    * bump version
    
    Signed-off-by: Charles Ferrell <charlie@manta.network>
    
    ---------
    
    Signed-off-by: Charles Ferrell <charlie@manta.network>

--- runtime/calamari/src/lib.rs
+++ runtime/calamari/src/lib.rs
@@ -143,10 +143,10 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("calamari"),
     impl_name: create_runtime_str!("calamari"),
     authoring_version: 2,
-    spec_version: 4710,
+    spec_version: 4720,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
-    transaction_version: 20,
+    transaction_version: 21,
     state_version: 0,
 };
 
--- runtime/manta/src/lib.rs
+++ runtime/manta/src/lib.rs
@@ -146,10 +146,10 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("manta"),
     impl_name: create_runtime_str!("manta"),
     authoring_version: 1,
-    spec_version: 4710,
+    spec_version: 4720,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
-    transaction_version: 9,
+    transaction_version: 10,
     state_version: 1,
 };
 
